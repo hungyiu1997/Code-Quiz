efabe6393207c414131524210d8949c767e88e97
Hungyiu Wong <hungyiuwong@Hungyius-MacBook-Air.local>
2021-08-30 14:53:43 -0400

    fixing assets

--- asset/script/script.js
+++ asset/script/script.js
@@ -1,120 +1,165 @@
 var startScreenEl = document.getElementById("startscreen");
 var startQuizBtn = document.getElementById("startquiz");
 var questionScreenEl = document.getElementById("questionscreen");
-var questionTextEl = document.getElementById("questiontext");
+var questionTextEl = document.getElementById("question-text");
 var choicesEl = document.getElementById("choices");
-var endScreenEl = document.getElementById("endscreen");
-var endScoreEl = document.getElementById("endscore");
-var initalsEl = document.getElementById("initials");
-var submitEl = document.getElementById("submit");
+var endScreen = document.getElementById("endscreen");
+var endScore = document.getElementById("endscore");
+var initialsEl = document.getElementById("initials");
+var submit = document.getElementById("submit");
 var timeEl = document.getElementById("time");
+var highScoreBtn = document.querySelector("#highscore-btn");
+var highScoreEl = document.querySelector('#highscore-page');
+var highScorePEl = document.querySelector('#highscore');
+var restartBtnEl = document.querySelector("#restart-quiz");
+var time = 60;
+var userChoice = "";
+var wins = 0;
+var losses = 0;
 var timerId;
 var currentQuestionIndex = 0;
 
 var quizQuestions = [
-    {
-        question:"When does a player receive a red card on pitch?",
-        choices:["When a player commit a fault", "When a player receive a second yellow card", "When a player conduct an unprofessional conduct", "All of the above."],
-        answer:"All of the above"
-    },
-    {
-        question:"When is video assistant referee (VAR) used?",
-        choices:["When there a clear and obvious error from the referee", "When a player commit a fault", "When there is a penalty shout", "When an on pitch player request it"],
-        answer:"When there is a clear and obvious error from the referee"
-    },
-    {
-        question:"Which one is not a current European club tournament?",
-        choices:["Champions League", "Europa League", "European Conference League", "UEFA Cup"],
-        answer:"UEFA Cup"
-    },
-    {
-        question:"When was the last FIFA World Cup hosted?",
-        choices:["Russia", "Germany", "Brazil", "Chlie"],
-        answer:"Russian"
-    },
-    {
-        question:"Who was the winner of the 2014 FIFA World Cup in Brazil?",
-        choices:["Brazil", "Germany", "Argentina", "The Netherlands"],
-        answer:["Germany"]
-    },
+  {
+    question: "When does a player receive a red card on pitch?",
+    choices: [
+      "When a player commit a fault",
+      "When a player receive a second yellow card",
+      "When a player conduct an unprofessional conduct",
+      "All of the above.",
+    ],
+    answer: "All of the above.",
+  },
+  {
+    question: "When is video assistant referee (VAR) used?",
+    choices: [
+      "When there a clear and obvious error from the referee",
+      "When a player commit a fault",
+      "When there is a penalty shout",
+      "When an on pitch player request it",
+    ],
+    answer: "When there is a clear and obvious error from the referee",
+  },
+  {
+    question: "Which one is not a current European club tournament?",
+    choices: [
+      "Champions League",
+      "Europa League",
+      "European Conference League",
+      "UEFA Cup",
+    ],
+    answer: "UEFA Cup",
+  },
+  {
+    question: "When was the last FIFA World Cup hosted?",
+    choices: ["Russia", "Germany", "Brazil", "Chlie"],
+    answer: "Russian",
+  },
+  {
+    question: "Who was the winner of the 2014 FIFA World Cup in Brazil?",
+    choices: ["Brazil", "Germany", "Argentina", "The Netherlands"],
+    answer: ["Germany"],
+  },
 ];
 console.log(quizQuestions);
 
-function startQuiz(){
-    //hide our startscreen
-    startScreen.setAttribute("class", "hide");
-    //show our question screen
-    questionScreen.removeAttribute("class", "hide");
-    //start timer
-    timeId = setInterval(timer,10000)
-    timeEl.textContent = secondsLeft
-    //call the next function
-    getQuestion();
+function startQuiz() {
+  console.log("startQuiz");
+  startScreenEl.setAttribute("class", "hide");
+  questionScreenEl.removeAttribute("class", "hide");
+  currentQuestionIndex = 0;
+  time = 60;
+  getQuestion();
+  startTimer();
 }
 
-startQuizBtn.addEventListener("click", startquiz);
+startQuizBtn.addEventListener("click", startQuiz);
 
 function getQuestion() {
-    //set variable for current question
-    var currentQuestion(quizQuestions);
-// add question to the page
+  console.log("getQuestion");
+  
+  choicesEl.innerHTML = "";
+  var quizObj = quizQuestions[currentQuestionIndex];
+  console.log("==== Question #", currentQuestionIndex + 1, "======");
+  console.log(quizObj);
+  questionTextEl.textContent = quizObj.question;
 
-//add the choices to the pages as buttons
-    quizQuestions.forEach(choices);
-//use .forEach method here
-//append to the page
-    document.append(questionScreen);
+  console.log(quizObj);
+  questionTextEl.textContent = quizObj.question;
+
+  for (var i = 0; i < quizObj.choices.length; i++) {
+    var btnEl = document.createElement("button");
+    //creating a DOM element assigned to p
+    btnEl.textContent = quizObj.choices[i];
+    //create the element for the ordered list
+    var liEl = document.createElement("li");
+    //attach the button to the list
+    liEl.appendChild(btnEl);
+    choicesEl.append(liEl);
+    //attach the element to the id:btnEl
+  } 
 }
 
+function handleChoiceBtn(event) {
+    var btnClickedEl = event.target;
+    console.log(btnClickedEl);
+    var quizObj = quizQuestions[currentQuestionIndex];
+    console.log(btnClickedEl.textContent);
+    console.log(quizObj);
+    console.log(quizObj.answer);
+    if (quizObj.answer === btnClickedEl.textContent){
+        wins++; 
+        console.log(wins);
+    } else {
+      losses++;
+      time = time-10;
+      timeEl.textContent = time;
+      console.log("losses", losses);
+    }
 
-//user question click function
-function answerQuestion() {
-    if (quizQuestions.choices === quizQuestions.answer);
-        console.log("You're correct")
-} else if (quizQuestions.choices !=== quizQuestions.answer);
-        console.log("You're Wrong")
-};
-    //check for right answer
-    //tell them if they're right or wrong
+    currentQuestionIndex++;
+    if (currentQuestionIndex >= quizQuestions.length) {
+      scoreSummary();
+    } else {
+    getQuestion();
+  }
+}
+function scoreSummary () {
+  clearInterval(counter);
+  console.log("Game Over!");
+  questionScreenEl.setAttribute("class", "hide");
+  endScreen.removeAttribute("class", "hide");
+} 
 
-    //move on to next question
-
-
-//end quiz function
-        //similar to start quiz
-function endQuiz(){
-    endScreen.setAttribute("class", "hide");
-    
+function startTimer() {
+  counter = setInterval(function() {
+    time--;
+    timeEl.textContent = time;
+    if (time<=0) {
+      scoreSummary();
+    }
+  },1000) 
 }
 
+document.getElementById('choices').addEventListener("click", handleChoiceBtn);
+submit.addEventListener("click", saveScore);
 
-
-function setTime() {
-    var timerInterval = setInterval(
-        function() {
-            secondsLeft--;
-            timeEl.textContent = secondsLeft + " seconds left!";
-
-            if(secondsLeft === 0) {
-                clearInterval(timerInterval);
-                sendMessage();
-            }
-        },
-        1000);
+function saveScore () {
+  var initials = initialsEl.value
+  console.log(initials, time);
+  var score = `${initials} : ${time}`;
+  localStorage.setItem("Score", score);
+  displayHighScore();
 }
 
-function sendMessage() {
-    timeEl.textContent= "You are out of time!"
-    return;
+highScoreBtn.addEventListener("click", displayHighScore);
 
+function displayHighScore () {
+  endScreen.setAttribute("class", "hide");
+  highScoreEl.removeAttribute("class", "hide");
+  var highScore = localStorage.getItem("Score");
+  highScorePEl.textContent = highScore;
+  
 }
-setTime();
- //timer function
-    //set the time
-    //watch for timer to get below 0
 
-var score = 0;
-//highscore functions
-
-    
-}
+restartBtnEl.addEventListener("click", startQuiz);
